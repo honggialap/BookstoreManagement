f24217993e2ddaa8f44b4606893cffc2b12ce879
NearHuscarl <16520846@gm.uit.edu.vn>
2018-06-26 22:13:00 +0700

    0.0.9: update import form (prototype)

--- src/BookstoreManagement.BUS/ImportDetailBUS.vb
+++ src/BookstoreManagement.BUS/ImportDetailBUS.vb
@@ -16,6 +16,10 @@ Public Class ImportDetailBUS
 		parameterBUS = New ParameterBUS()
 	End Sub
 
+	Public Function getNextId(ByRef nextId As Integer) As Result
+		Return importDetailDAL.getNextId(nextId)
+	End Function
+
 	Public Function isValidImportDetail(importDetail As ImportDetailDTO)
 		Dim parameter As ParameterDTO
 		Dim result = parameterBUS.selectAll(parameter)
@@ -34,19 +38,31 @@ Public Class ImportDetailBUS
 		Return False
 	End Function
 
-	Public Function insertAll(importDetails As List(Of ImportDetailDTO)) As Result
-		Return importDetailDAL.insertAll(importDetails)
+	Public Function selectAll(ByRef importDetails As List(Of ImportDetailDTO)) As Result
+		Return importDetailDAL.selectALL(importDetails)
 	End Function
 
-	Public Function getNextId(ByRef nextId As Integer) As Result
-		Return importDetailDAL.getNextId(nextId)
+	Public Function selectAll_ByImport(importID As String, ByRef importDetails As List(Of ImportDetailDTO)) As Result
+		'Return importDetailDAL.selectALL_ByImport(importID, importDetails)
+
+		' TODO: remove
+		importDetails = New List(Of ImportDetailDTO)
+		importDetails.Add(New ImportDetailDTO("IMPORTDETAIL000", "IMPORT00", "BOOK001", 50, 30, 50))
+		importDetails.Add(New ImportDetailDTO("IMPORTDETAIL001", "IMPORT00", "BOOK002", 40, 100, 60))
+		importDetails.Add(New ImportDetailDTO("IMPORTDETAIL002", "IMPORT00", "BOOK003", 20, 300, 40))
+
+		Return New Result(True)
+	End Function
+
+	Public Function insertAll(importDetails As List(Of ImportDetailDTO)) As Result
+		Return importDetailDAL.insertAll(importDetails)
 	End Function
 
 	Public Function update(importDetail As ImportDetailDTO) As Result
 		Return importDetailDAL.update(importDetail)
 	End Function
 
-	Public Function delete(importDetail As String) As Result
-		Return importDetailDAL.delete(importDetail)
+	Public Function delete(importDetailID As String) As Result
+		Return importDetailDAL.delete(importDetailID)
 	End Function
 End Class
--- src/BookstoreManagement.GUI/frmImport.vb
+++ src/BookstoreManagement.GUI/frmImport.vb
@@ -1,55 +1,163 @@
-﻿Imports BookstoreManagement.BUS
+﻿Imports System.ComponentModel
+Imports BookstoreManagement.BUS
 Imports BookstoreManagement.DTO
+Imports MetroFramework
 Imports Utility
 
 Public Class frmImport
+	Private isImportLoading As Boolean
+	Private isImportDetailLoading As Boolean
+
 	Private importBUS As ImportBUS
 	Private importDetailBUS As ImportDetailBUS
+	Private bookBUS As BookBUS
+
+	''' <summary>
+	''' If selected import ID = nextImportID, enable add button, otherwise enable update button
+	''' </summary>
+	Private nextImportID As String
+
+	''' <summary>
+	''' List of ImportDetailDTO that has changed since last add/update
+	''' </summary>
+	Private importDetailsToUpdate As List(Of ImportDetailDTO) = New List(Of ImportDetailDTO)
+
+	Public Sub New()
+
+		' This call is required by the designer.
+		InitializeComponent()
+
+		' Add any initialization after the InitializeComponent() call.
+
+	End Sub
 
 	Private Sub frmImport_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 		importBUS = New ImportBUS()
 		importDetailBUS = New ImportDetailBUS()
-		ShowNextID()
+		bookBUS = New BookBUS()
+
+		LoadImport()
 	End Sub
 
-	Private Sub ShowNextID()
-		Dim nextId As Integer
+	Private Sub LoadImport()
+		isImportLoading = True
+
+		Dim _imports As List(Of ImportDTO) = New List(Of ImportDTO)
 		Dim result As Result
 
-		result = importDetailBUS.getNextId(nextId)
+		result = importBUS.selectALL(_imports)
 
-		If (result.FlagResult = True) Then
-			dgvImport.Rows.Add({0, nextId.ToString(), Nothing, Nothing, Nothing, Nothing})
+		If (result.FlagResult = True) Then ' Add next Import ID
+			result = importBUS.getNextId(nextImportID)
+
+			If (result.FlagResult = True) Then
+				_imports.Add(New ImportDTO(nextImportID, Date.Now.ToShortDateString()))
+
+				'TODO: Remove the lambda after do the trimming part in db
+				_imports = _imports.OrderByDescending(Function(import) import.ID).ToList()
+
+				For Each _import As ImportDTO In _imports
+					Dim currentIndex = dgvImport.Rows.Add()
+
+					dgvImport.Rows(currentIndex).Cells("colImportID").Value = _import.ID
+					dgvImport.Rows(currentIndex).Cells("colImportDate").Value = _import.ImportDate.ToShortDateString()
+				Next
+			Else
+				MessageBox.Show("Cannot get next ID of import", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+				Console.WriteLine(result.SystemMessage)
+			End If
 		Else
-			MessageBox.Show("Cannot get the next ID", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+			MessageBox.Show("Cannot load import", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
 			Console.WriteLine(result.SystemMessage)
 		End If
+
+		isImportLoading = False
 	End Sub
+
+	Private Sub LoadImportDetail(importID As String)
+		isImportDetailLoading = True
+
+		Dim importDetails As List(Of ImportDetailDTO) = New List(Of ImportDetailDTO)
+		Dim result As Result
+
+		result = importDetailBUS.selectAll_ByImport(importID, importDetails)
+
+		If (result.FlagResult = False) Then
+			importDetails.Add(New ImportDetailDTO(0, 0, Nothing, Nothing, Nothing, Nothing))
+		End If
+
+		LoadBooks()
+
+		For Each importDetail As ImportDetailDTO In importDetails
+			Dim currentIndex = dgvImportDetail.Rows.Add()
+
+			dgvImportDetail.Rows(currentIndex).Cells("colImportDetailID").Value = importDetail.ID
+			dgvImportDetail.Rows(currentIndex).Cells("colBookID").Value = importDetail.BookID
+			dgvImportDetail.Rows(currentIndex).Cells("colImportAmount").Value = importDetail.ImportAmount
+			dgvImportDetail.Rows(currentIndex).Cells("colCurrentAmount").Value = importDetail.CurrentAmount
+			dgvImportDetail.Rows(currentIndex).Cells("colImportPrice").Value = importDetail.ImportPrice
+		Next
+
+		isImportDetailLoading = False
+	End Sub
+
+	Private Sub LoadBooks()
+		isImportLoading = True
+
+		Dim books As List(Of BookDTO) = New List(Of BookDTO)
+		Dim result As Result
+
+		result = bookBUS.selectAll(books)
+
+		'TODO: Remove the lambda after do the trimming part in db
+		books = books.OrderByDescending(Function(import) import.ID).Select(
+		Function(import)
+			import.ID = import.ID.Trim()
+			Return import
+		End Function).ToList()
+
+		If (result.FlagResult = True) Then
+			colBookID.DataSource = books
+			colBookID.ValueMember = "ID"
+			colBookID.DisplayMember = "Name"
+		Else
+			MessageBox.Show("Cannot load books", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+			Console.WriteLine(result.SystemMessage)
+		End If
+
+		isImportLoading = False
+	End Sub
+
+	Private Function GetImportDetailFromCells(cells As DataGridViewCellCollection) As ImportDetailDTO
+		Dim importDetail As ImportDetailDTO = New ImportDetailDTO()
+
+		importDetail.ID = cells("colImportDetailID").Value
+		importDetail.BookID = cells("colBookID").Value
+		importDetail.ImportAmount = cells("colImportAmount").Value
+		importDetail.CurrentAmount = cells("colCurrentAmount").Value
+		importDetail.ImportPrice = cells("colImportPrice").Value
+
+		Return importDetail
+	End Function
 
 	Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
 		Dim importDTO = New ImportDTO()
-		Dim importDetailDTOs = New List(Of ImportDetailDTO)
+		Dim importDetails = New List(Of ImportDetailDTO)
 
-		For Each row As DataGridViewRow In dgvImport.Rows
+		'txtImportID.Text = dgvImportDetail.Rows(0).Cells("ImportID").Value
+
+		For Each row As DataGridViewRow In dgvImportDetail.Rows
 			If (row.IsNewRow) Then
 				Exit For
 			End If
 
-			Dim importDetailDTO As ImportDetailDTO = New ImportDetailDTO()
-
-			importDetailDTO.ID = row.Cells("ID").Value
-			importDetailDTO.ImportID = row.Cells("ImportID").Value
-			importDetailDTO.BookID = row.Cells("BookID").Value
-			importDetailDTO.ImportAmount = row.Cells("ImportAmount").Value
-			importDetailDTO.CurrentAmount = row.Cells("CurrentAmount").Value
-			importDetailDTO.ImportPrice = row.Cells("ImportPrice").Value
-
-			importDetailDTOs.Add(importDetailDTO)
+			Dim importDetail As ImportDetailDTO = GetImportDetailFromCells(row.Cells)
+			importDetails.Add(importDetail)
 		Next
 
 		Dim result As Result
 
-		importDTO.DateImport = dtpReceivedDate.Value
+		'importDTO.DateImport = dtpReceivedDate.Value
 		result = importBUS.insert(importDTO)
 
 		If (result.FlagResult = True) Then
@@ -59,7 +167,7 @@ Public Class frmImport
 			Console.WriteLine(result.SystemMessage)
 		End If
 
-		result = importDetailBUS.insertAll(importDetailDTOs)
+		result = importDetailBUS.insertAll(importDetails)
 
 		If (result.FlagResult = True) Then
 			MessageBox.Show("Import detail is added", "Status", MessageBoxButtons.OK, MessageBoxIcon.Information)
@@ -67,12 +175,106 @@ Public Class frmImport
 			MessageBox.Show("Failed to add import detail", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
 			Console.WriteLine(result.SystemMessage)
 		End If
+
+		LoadImport() ' Reload import to reflect new changes and add nextId
 	End Sub
 
-	Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
-		dgvImport.Rows.Clear()
-		dgvImport.Refresh()
+	Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+		Dim importDetails = New List(Of ImportDetailDTO)
 
-		ShowNextID()
+		'txtImportID.Text = dgvImportDetail.Rows(0).Cells("ImportID").Value
+
+		For Each row As DataGridViewRow In dgvImportDetail.Rows
+			If (row.IsNewRow) Then
+				Exit For
+			End If
+
+			Dim importDetail As ImportDetailDTO = GetImportDetailFromCells(row.Cells)
+			importDetails.Add(importDetail)
+		Next
+
+		Dim result As Result
+
+		For Each importDetail As ImportDetailDTO In importDetailsToUpdate
+			result = importDetailBUS.update(importDetail)
+
+			If (result.FlagResult = True) Then
+				MessageBox.Show("Import detail is updated", "Status", MessageBoxButtons.OK, MessageBoxIcon.Information)
+			Else
+				MessageBox.Show("Failed to update import detail", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+				Console.WriteLine(result.SystemMessage)
+				Exit For
+			End If
+		Next
+
+		LoadImportDetailsFromSelectedImport()
+		importDetailsToUpdate.Clear()
+
+		btnUpdate.Enabled = False
+	End Sub
+
+	Private isChangingImportSelection As Boolean = False
+	Private lastImportSelectionIndex As Integer
+	Private Sub dgvImport_SelectionChanged(sender As Object, e As EventArgs) Handles dgvImport.SelectionChanged
+		If (isChangingImportSelection) Then
+			Return
+		End If
+
+		If (btnUpdate.Enabled) Then 'Detect change in ImportDetail. warn to override
+			isChangingImportSelection = True
+
+			Dim dialogResult = MetroMessageBox.Show(Me, "All changed in Import Detail will be overwrite. Do you want to switch?",
+										"MetroMessagebox", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+
+			If (dialogResult = DialogResult.Yes) Then
+				LoadImportDetailsFromSelectedImport()
+			Else
+				dgvImport.Rows(lastImportSelectionIndex).Selected = True
+			End If
+		Else
+			LoadImportDetailsFromSelectedImport()
+		End If
+
+		lastImportSelectionIndex = dgvImport.CurrentCell.RowIndex
+		isChangingImportSelection = False
+	End Sub
+
+	Private Sub LoadImportDetailsFromSelectedImport()
+		Dim selectedImportID = dgvImport.Rows(dgvImport.CurrentRow.Index).Cells("colImportID").Value
+
+		If (selectedImportID Is Nothing) Then
+			Return
+		End If
+
+		If (selectedImportID = nextImportID) Then
+			btnAdd.Enabled = True
+		Else
+			btnAdd.Enabled = False
+		End If
+		btnUpdate.Enabled = False
+
+		dgvImportDetail.Rows.Clear()
+		dgvImportDetail.Refresh()
+
+		LoadImportDetail(selectedImportID)
+	End Sub
+
+	Private Sub dgvImportDetail_CellValueChanged(sender As Object, e As DataGridViewCellEventArgs) Handles dgvImportDetail.CellValueChanged
+		If (e.RowIndex = -1 Or isImportDetailLoading) Then
+			Return
+		End If
+
+		Dim changedImportDetail = GetImportDetailFromCells(dgvImportDetail.Rows(e.RowIndex).Cells)
+		Dim duplicatedItems = importDetailsToUpdate.
+			Where(Function(detail) detail.ID = changedImportDetail.ID)
+
+		If (duplicatedItems.Count = 0) Then
+			importDetailsToUpdate.Add(changedImportDetail)
+		Else
+			Dim oldIndex = importDetailsToUpdate.IndexOf(duplicatedItems.First())
+			importDetailsToUpdate(oldIndex) = changedImportDetail
+		End If
+
+		btnUpdate.Enabled = True
 	End Sub
 End Class
